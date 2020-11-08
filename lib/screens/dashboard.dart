7222e07f333a1255acb9251a44c678583968c09f
smv1999 <vaidhyanathan.sm@gmail.com>
2020-11-08 23:02:36 +0530

    changed career_guidance image

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -79,7 +79,7 @@ class _DashboardState extends State<Dashboard> {
                         onTap: () {},
                         child: Card(
                           clipBehavior: Clip.antiAlias,
-                          child: Image.asset('images/career_guidance.jpg'),
+                          child: Image.asset('images/career_guidance.png'),
                           // Text('Career Guidance'),
                           elevation: 5,
                         ),
