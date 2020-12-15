b83e89bbc9720df03ead02a15e842b40438821d9
smv1999 <vaidhyanathan.sm@gmail.com>
2020-12-16 00:44:10 +0530

    implemented Project Ideas successfully with the CRD operations

--- lib/screens/projects_ideas.dart
+++ lib/screens/projects_ideas.dart
@@ -20,12 +20,14 @@ class _ProjectIdeasState extends State<ProjectIdeas> {
   DatabaseReference myProjRef;
   Auth auth = new Auth();
   Future f;
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
     f = retrieveProjectData();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -63,8 +65,7 @@ class _ProjectIdeasState extends State<ProjectIdeas> {
             child: Text(
               'Scribblet - Save your project ideas!',
               style: TextStyle(
-                  fontSize: 25.0,
-                  fontStyle: FontStyle.italic,
+                  fontSize: 20.0,
                   color: Colors.black,
                   fontFamily: 'MyFont',
                   fontWeight: FontWeight.bold),
@@ -77,11 +78,38 @@ class _ProjectIdeasState extends State<ProjectIdeas> {
                 return ListView.builder(
                   itemCount: snapshot.data.length,
                   itemBuilder: (context, index) {
-                    return ListTile(
-                      subtitle: Text(snapshot.data[index].projectDescription),
-                      title: Text(
-                        snapshot.data[index].projectTitle,
-                        style: GoogleFonts.ptSansNarrow(),
+                    return Dismissible(
+                      key: UniqueKey(),
+                      child: Card(
+                        margin: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          subtitle:
+                              Text(snapshot.data[index].projectDescription),
+                          title: Text(
+                            snapshot.data[index].projectTitle,
+                            style: GoogleFonts.ptSansNarrow(),
+                          ),
+                        ),
+                      ),
+                      onDismissed: (direction) {
+                        // Remove the item from the data source.
+                        setState(() {
+                          String delTitle = snapshot.data[index].projectTitle;
+                          String delDescription =
+                              snapshot.data[index].projectDescription;
+                          snapshot.data.removeAt(index);
+                          deleteProjectData(delTitle, context, delDescription);
+                        });
+                      },
+                      // Show a red background as the item is swiped away.
+                      background: Container(
+                        padding: EdgeInsets.all(20.0),
+                        alignment: Alignment.centerRight,
+                        color: Colors.grey,
+                        child: Icon(
+                          Icons.delete,
+                          color: Colors.white,
+                        ),
                       ),
                     );
                   },
@@ -107,7 +135,7 @@ class _ProjectIdeasState extends State<ProjectIdeas> {
     Dialog errorDialog = Dialog(
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
       child: Container(
-          height: 400.0,
+          height: 310.0,
           width: 400.0,
           child: Padding(
             padding: const EdgeInsets.all(10.0),
@@ -118,7 +146,7 @@ class _ProjectIdeasState extends State<ProjectIdeas> {
                 children: [
                   Center(
                     child: Text(
-                      'New Project Idea',
+                      'New Project Idea 💡',
                       style: GoogleFonts.ptSansNarrow(
                         textStyle: TextStyle(
                             fontWeight: FontWeight.bold, fontSize: 20.0),
@@ -236,19 +264,71 @@ class _ProjectIdeasState extends State<ProjectIdeas> {
 
     myProjRef =
         FirebaseDatabase.instance.reference().child('Projects').child(userId);
-      myProjRef.once().then((DataSnapshot dataSnapshot) {
-        setState(() {
-          for (var snapshot in dataSnapshot.value.values) {
-             Projects project =
+    myProjRef.once().then((DataSnapshot dataSnapshot) {
+      setState(() {
+        for (var snapshot in dataSnapshot.value.values) {
+          Projects project =
               Projects(snapshot["title"], snapshot["description"]);
 
           projects.add(project);
         }
-                return projects;
+        return projects;
+      });
+    });
+    return projects;
+  }
 
-        });
-        
-      });    
-      return projects;
+  deleteProjectData(
+      var delTitle, BuildContext context, var delDescription) async {
+    final FirebaseUser user = await auth.getCurrentUser();
+    final userId = user.uid;
+    FirebaseDatabase.instance
+        .reference()
+        .child('Projects')
+        .child(userId)
+        .once()
+        .then((DataSnapshot dataSnapshot) {
+      for (var snapshot in dataSnapshot.value.values) {
+        if (snapshot["title"] == delTitle) {
+          Scaffold.of(context).showSnackBar(SnackBar(
+            content: Text(snapshot["title"] + " deleted"),
+            action: SnackBarAction(
+                label: "UNDO",
+                onPressed: () => setState(
+                      () => {undoDeleteProject(delTitle, delDescription)},
+                    ) // this is what you needed
+                ),
+          ));
+          var projectKey = dataSnapshot.value.keys.firstWhere(
+              (k) => dataSnapshot.value[k] == snapshot,
+              orElse: () => null);
+          FirebaseDatabase.instance
+              .reference()
+              .child('Projects')
+              .child(userId)
+              .child(projectKey)
+              .remove();
+        }
+      }
+    });
+  }
+
+  undoDeleteProject(var delTitle, var delDescription) async {
+    projectTitle = delTitle;
+    projectDescription = delDescription;
+    FirebaseUser user = await auth.getCurrentUser();
+    final userId = user.uid;
+    myProjRef =
+        FirebaseDatabase.instance.reference().child('Projects').child(userId);
+
+    HashMap<String, String> projectMap = new HashMap();
+    projectMap.putIfAbsent('title', () => projectTitle);
+    projectMap.putIfAbsent('description', () => projectDescription);
+
+    myProjRef.push().set(projectMap);
+
+    Toast.show("Undo Successful!", context,
+        duration: Toast.LENGTH_LONG, gravity: Toast.BOTTOM);
+    f = retrieveProjectData();
   }
 }
